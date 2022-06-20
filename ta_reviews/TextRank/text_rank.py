84f8dbf31487b2e83289e2733425bbefefe030d1
AlwaysBeMyDay1 <eodus09011@naver.com>
2022-06-20 22:27:38 +0900

    TextRank refactoring

--- ta_reviews/TextRank/text_rank.py
+++ ta_reviews/TextRank/text_rank.py
@@ -12,40 +12,34 @@ def load_json(path):
     with open(path) as json_file:
         return json.load(json_file)
 
+def text_rank_with_word_count(text, word_count):
+    try:
+        result = summarize(text["detail"], word_count=word_count)
+    except:
+        result = ""
+    return result
+
+def text_rank_with_ratio(text, ratio):
+    try:
+        result = summarize(text["detail"], ratio=ratio)
+    except:
+        result = ""
+    return result
+
 def text_rank():
     cities = load_json("tripadvisor_reviews.json")
     result_list = []
     for city in cities:
         print(cities.index(city))
         for review in city["reviews"]:
-            try:
-                review["after_text_rank_10words"] = summarize(review["detail"], word_count=10)
-            except:
-                review["after_text_rank_10words"] = ""
-            try:
-                review["after_text_rank_5words"] = summarize(review["detail"], word_count=5)
-            except:
-                review["after_text_rank_5words"] = ""
-            try:
-                review["after_text_rank_1words"] = summarize(review["detail"], word_count=1)
-            except:
-                review["after_text_rank_1words"] = ""
-            try:
-                review["after_text_rank_02ratio"] = summarize(review["detail"], ratio=0.2)
-            except:
-                review["after_text_rank_02ratio"] = ""
-            try:
-                review["after_text_rank_01ratio"] = summarize(review["detail"], ratio=0.1)
-            except:
-                review["after_text_rank_01ratio"] = ""
-            try:
-                review["after_text_rank_001ratio"] = summarize(review["detail"], ratio=0.01)
-            except:
-                review["after_text_rank_001ratio"] = ""
+            review["after_text_rank_10words"] = text_rank_with_word_count(text=review, word_count=10)
+            review["after_text_rank_5words"] = text_rank_with_word_count(text=review, word_count=5)
+            review["after_text_rank_3words"] = text_rank_with_word_count(text=review, word_count=3)
+            review["after_text_rank_02ratio"] = text_rank_with_ratio(text=review, ratio=0.2)
+            review["after_text_rank_01ratio"] = text_rank_with_ratio(text=review, ratio=0.1)
+            review["after_text_rank_001ratio"] = text_rank_with_ratio(text=review, ratio=0.01)
         result_list.append(city)
-    
     save_json(result_list, "tripadvisor_reviews_textranked.json")
-
 
 
 text_rank()
